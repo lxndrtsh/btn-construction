b1307896a08965aa38699e373951180bcb2c3dd9
lxndrtsh <lxndr.tsh@gmail.com>
2014-07-16 10:49:50 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>By The Nerds | Kansas City Nerd Collective</title>
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+ <video autoplay loop muted id="bgvid">
+	<source src="video/wgo.mp4" type="video/mp4">
+	<source src="video/wgo.webm" type="video/webm">
+</video>
+
+<div class="overlay">
+	<h1>We're too busy enjoying this tasty video, but we'll create a real website soon.</h1>
+	<h2>- The Nerds</h2>
+</div>
+ 
+</body>
+</html>
